7f876de1a4b4f69c0c558f617ee930ab8487199e
Luis Quezada <luis.quezada881213@gmail.com>
2020-03-29 18:03:20 -0600

    data update on city population

--- components/Component.CountryPopulation.jsx
+++ components/Component.CountryPopulation.jsx
@@ -4,13 +4,13 @@ import * as d3 from 'd3'
 function showData(selectedCountry) {
 
     if (selectedCountry) {
-        
+
         let normPopData = []
 
         Object.keys(selectedCountry.population).map(element => {
-            if(+element){
+            if (+element) {
                 normPopData.push({
-                    "year" : element,
+                    "year": element,
                     "population": Number(selectedCountry.population[element].replace(/,/g, ''))
                 })
             }
@@ -18,11 +18,44 @@ function showData(selectedCountry) {
 
         console.log(normPopData)
 
-        let body = d3.select(".country-population-body")
+        let body = d3.select("#country-population-body")
 
         let max = d3.max(normPopData, d => d.population)
 
         console.log(max)
+
+        let widthScale = d3.scaleLinear()
+            .range([0, 500])
+            .domain([0, max])
+
+        let domain = normPopData.map(d => d.year)
+
+        console.log(domain)
+
+        let positionScale = d3.scaleBand()
+            .range([0, 500])
+            .domain(domain)
+            .padding(0.3)
+
+        let join = body
+            .selectAll("rect")
+            .data(normPopData)
+
+        let newElements = join.enter()
+            .append("rect")
+            .attr("fill", "blue")
+            .attr("width", d => widthScale(d.population))
+            .attr("height", positionScale.bandwidth())
+            .attr("y", d => positionScale(d.year))
+        
+        join.merge(newElements)
+            .attr("fill", "blue")
+            .attr("width", d => widthScale(d.population))
+            .attr("height", positionScale.bandwidth())
+            .attr("y", d => positionScale(d.year))
+
+        join.exit().remove()
+        
     }
 }
 
