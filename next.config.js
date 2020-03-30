20244179e2e28d94d02ca8dcca51567ae585e1c3
Luis Quezada <luis.quezada881213@gmail.com>
2020-03-29 23:43:52 -0600

    height modify

--- next.config.js
+++ next.config.js
@@ -1,8 +1,10 @@
 const withSass = require('@zeit/next-sass');
 const withCSS = require('@zeit/next-css');
+const isProd = process.env.NODE_ENV === 'production'
 
 module.exports = withSass({
     env: {
-        REACT_APP_API_COUNTRIES: 'https://api.myjson.com/bins/1fomjs',
-    }
+        REACT_APP_API_COUNTRIES: 'https://api.myjson.com/bins/ldij8',
+    },
+    assetPrefix: isProd ? 'https://luisquezada881213.github.io/world-data/' : '',
 })
---
+++ out/_next/static/chunks/styles.5e717c6855545d8f9093.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[9],{pRDQ:function(n,p,w){}}]);
