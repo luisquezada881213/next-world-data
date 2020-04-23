cb66c142499c245620638d92c146421f3ca97926
Luis Quezada <luis.quezada881213@gmail.com>
2020-04-22 23:13:46 -0500

    logo powered by

--- components/layouts/Main.jsx
+++ components/layouts/Main.jsx
@@ -1,17 +1,19 @@
 import { Provider } from 'react-redux'
 import store from '../../redux/store'
 
-const layoutStyle = {
-  margin: 0,
-  padding: 0,
-  height: '100%',
-  width: '100%',
-  overflow: 'hidden',
+const styles = {
+  layoutStyle:{
+    margin: 0,
+    padding: 0,
+    height: '100%',
+    width: '100%',
+    overflow: 'hidden'}
 };
 
 const Layout = props => (
   <Provider store={store}>
-    <div className='main-layout' style={layoutStyle}>
+    <div className='main-layout' style={styles.layoutStyle}>
+      <div className={'powered-label'}><a href='https://graphcms.com/'>Powered by<img src={'/graphcms.png'} alt='graphcms'/></a></div>
       {props.children}
     </div>
   </Provider>
